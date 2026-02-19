97fc34f691b982664e3e202e7d53367bb78ee8bb
ktmgw486 <ktmgw486@gmail.com>
2026-02-19 05:16:25 +0000

    #15 chore: cooment 부분 오류 수정완료

--- backend/src/classes/dtos/comment.request.dto.ts
+++ backend/src/classes/dtos/comment.request.dto.ts
@@ -2,9 +2,17 @@ export class CommentRequestDto {
   content!: string;
   userId!: string;
   taskId!: string;
+  constructor(data: { content: string; userId: string; taskId: string }) {
+    this.content = data.content;
+    this.userId = data.userId;
+    this.taskId = data.taskId;
+  }
 }
 
 export class CommentUpdateRequestDto {
   content: string | undefined;
+  constructor(data: { content: string }) {
+    this.content = data.content;
+  }
 }
 export default { CommentRequestDto, CommentUpdateRequestDto };
